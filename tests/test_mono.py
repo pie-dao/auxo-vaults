9dd48668105d7d9f7565f1562360f9aa9ed53f8b
dantop114 <dantop114@protonmail.com>
2022-01-14 00:43:41 +0100

    Adds tests

--- tests/test_mono.py
+++ tests/test_mono.py
@@ -84,85 +84,83 @@ def test_withdraw_strategy(deployer, token, vault, strategy, mock_accounts):
     assert token.balanceOf(vault) == 1000 * 1e18
     assert token.balanceOf(strategy) == 2000 * 1e18
 
-# def test_batch_burning(deployer, token, vault, strategy, mock_accounts):
-#     users = mock_accounts[:3]
-#     vault.trustStrategy(strategy, {'from': deployer})
-#     vault.setWithdrawalQueue([strategy], {'from': deployer})
+def test_batch_burning(deployer, token, vault, strategy, mock_accounts):
+    users = mock_accounts[:3]
+    vault.trustStrategy(strategy, {'from': deployer})
+    vault.setWithdrawalQueue([strategy], {'from': deployer})
 
-#     for user in users:
-#         token.mint(user.address, 1000 * 1e18, {'from': deployer})
+    for user in users:
+        token.mint(user.address, 1000 * 1e18, {'from': deployer})
         
-#         token.approve(vault, 1000 * 1e18, {'from': user})
-#         vault.deposit(1000 * 1e18, {'from': user})
+        token.approve(vault, 1000 * 1e18, {'from': user})
+        vault.deposit(user.address, 1000 * 1e18, {'from': user})
     
-#     vault.depositIntoStrategy(strategy, token.balanceOf(vault), {'from': deployer})
+    vault.depositIntoStrategy(strategy, token.balanceOf(vault), {'from': deployer})
 
-#     for user in users:
-#         balance = vault.balanceOf(user)
-#         vault.enterBatchBurn(balance, {'from': user})
+    for user in users:
+        balance = vault.balanceOf(user)
+        vault.enterBatchBurn(balance, {'from': user})
     
-#     vault.execBatchBurn({'from': deployer})
+    vault.execBatchBurn({'from': deployer})
 
-#     for user in users:
-#         vault.exitBatchBurn({'from': user})
-#         assert token.balanceOf(user) == 1000 * 1e18
+    for user in users:
+        vault.exitBatchBurn({'from': user})
+        assert token.balanceOf(user) == 1000 * 1e18
 
-# def test_batch_burning_with_float(deployer, token, vault, strategy, mock_accounts):
-#     users = mock_accounts[:3]
-#     vault.trustStrategy(strategy, {'from': deployer})
-#     vault.setWithdrawalQueue([strategy], {'from': deployer})
+def test_batch_burning_with_float(deployer, token, vault, strategy, mock_accounts):
+    users = mock_accounts[:3]
+    vault.trustStrategy(strategy, {'from': deployer})
+    vault.setWithdrawalQueue([strategy], {'from': deployer})
 
-#     for user in users:
-#         token.mint(user.address, 1000 * 1e18, {'from': deployer})
+    for user in users:
+        token.mint(user.address, 1000 * 1e18, {'from': deployer})
         
-#         token.approve(vault, 1000 * 1e18, {'from': user})
-#         vault.deposit(1000 * 1e18, {'from': user})
+        token.approve(vault, 1000 * 1e18, {'from': user})
+        vault.deposit(user.address, 1000 * 1e18, {'from': user})
     
-#     vault.depositIntoStrategy(strategy, 2000 * 1e18, {'from': deployer})
+    vault.depositIntoStrategy(strategy, 2000 * 1e18, {'from': deployer})
 
-#     for user in users:
-#         balance = vault.balanceOf(user)
-#         vault.enterBatchBurn(balance, {'from': user})
+    for user in users:
+        balance = vault.balanceOf(user)
+        vault.enterBatchBurn(balance, {'from': user})
     
-#     vault.execBatchBurn({'from': deployer})
+    vault.execBatchBurn({'from': deployer})
 
-#     for user in users:
-#         vault.exitBatchBurn({'from': user})
-#         assert token.balanceOf(user) == 1000 * 1e18
+    for user in users:
+        vault.exitBatchBurn({'from': user})
+        assert token.balanceOf(user) == 1000 * 1e18
 
-# def test_batch_burning_with_loss(deployer, token, vault, strategy, mock_accounts):
-#     users = mock_accounts[:3]
-#     vault.trustStrategy(strategy, {'from': deployer})
-#     vault.setWithdrawalQueue([strategy], {'from': deployer})
+def test_batch_burning_with_loss(deployer, token, vault, strategy, mock_accounts):
+    users = mock_accounts[:3]
+    vault.trustStrategy(strategy, {'from': deployer})
+    vault.setWithdrawalQueue([strategy], {'from': deployer})
 
-#     for user in users:
-#         token.mint(user.address, 1000 * 1e18, {'from': deployer})
+    for user in users:
+        token.mint(user.address, 1000 * 1e18, {'from': deployer})
         
-#         token.approve(vault, 1000 * 1e18, {'from': user})
-#         vault.deposit(1000 * 1e18, {'from': user})
+        token.approve(vault, 1000 * 1e18, {'from': user})
+        vault.deposit(user.address, 1000 * 1e18, {'from': user})
     
-#     vault.depositIntoStrategy(strategy, 3000 * 1e18, {'from': deployer})
-#     strategy.simulateLoss(500 * 1e18, {'from': deployer})
+    vault.depositIntoStrategy(strategy, 3000 * 1e18, {'from': deployer})
+    strategy.simulateLoss(500 * 1e18, {'from': deployer})
 
-#     assert token.balanceOf(strategy) == 2500 * 1e18
+    assert token.balanceOf(strategy) == 2500 * 1e18
 
-#     vault.harvest([strategy], {'from': deployer})
+    vault.harvest([strategy], {'from': deployer})
 
-#     assert vault.totalStrategyHoldings() == 2500 * 1e18
-#     assert vault.lockedProfit() == 0
+    assert vault.totalStrategyHoldings() == 2500 * 1e18
+    assert vault.lockedProfit() == 0
 
-#     vault_balance = {}
-#     for user in users:
-#         vault_balance[user.address] = vault.balanceOf(user)
-#         vault.enterBatchBurn(vault_balance[user.address], {'from': user})
+    for user in users:
+        vault.enterBatchBurn(vault.balanceOf(user.address), {'from': user})
 
-#     vault.execBatchBurn({'from': deployer})
+    vault.execBatchBurn({'from': deployer})
 
-#     assert token.balanceOf(vault) == 2499999999999999999000
-#     assert vault.totalStrategyHoldings() == 1000
-#     assert vault.totalHoldings() == 1000
+    assert token.balanceOf(vault) == 2499999999999999999000
+    assert vault.totalUnderlying() == 2500000000000000000000
+    assert vault.totalStrategyHoldings() == 1000
 
-#     for user in users:
-#         vault.exitBatchBurn({'from': user})
-#         expected = 833333333333333333000 # ~ 2500 / 3
-#         assert token.balanceOf(user) == expected
+    for user in users:
+        vault.exitBatchBurn({'from': user})
+        expected = 833333333333333333000 # ~ 2500 / 3
+        assert token.balanceOf(user) == expected
