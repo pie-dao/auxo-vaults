aafeaa02895a7af64927a9a01d614d8ecaf4b634
dantop114 <dantop114@protonmail.com>
2022-03-30 19:11:39 +0200

    (tests): add tests - wip

--- vaults/tests/unit/vault/test_batch_burn.py
+++ vaults/tests/unit/vault/test_batch_burn.py
@@ -6,77 +6,69 @@ from brownie import ZERO_ADDRESS, MockStrategy, Vault
 MAX_UINT256 = 2**256 - 1
 
 
-def test_deposit_without_allowance(misc_accounts, gov, token, auth, Vault):
-    # mint some tokens to alice
-    alice = misc_accounts[0]
-    token.mint(alice, 10_000e18)
-
+def test_batched_burning(misc_accounts, gov, token, auth, Vault, MockStrategy):
     # deploy the vault
     vault = gov.deploy(Vault)
     vault.initialize(token, auth, ZERO_ADDRESS, ZERO_ADDRESS)
     vault.triggerPause()
     vault.setDepositLimits(MAX_UINT256, MAX_UINT256)
 
-    # this should revert
-    with brownie.reverts():
-        vault.deposit(alice, 10_000e18, {"from": alice})
+    for u in misc_accounts:
+        token.mint(u, 1000 * 1e18)
+
+        token.approve(vault, 1000 * 1e18, {"from": u})
+        vault.deposit(u, 1000 * 1e18, {"from": u})
+
+    for u in misc_accounts:
+        balance = vault.balanceOf(u)
+        vault.enterBatchBurn(balance, {"from": u})
+
+        receipt = vault.userBatchBurnReceipts(u).dict()
+
+        assert receipt["round"] == 1
+        assert receipt["shares"] == balance
+
+    total_shares = vault.totalSupply()
+
+    vault.execBatchBurn()
+
+    batch_burn = vault.batchBurns(1).dict()
+    assert batch_burn["totalShares"] == total_shares
+    assert batch_burn["amountPerShare"] == 1e18
+
+    for u in misc_accounts:
+        vault.exitBatchBurn({"from": u})
+        assert token.balanceOf(u) == 1000 * 1e18
 
 
-def test_deposit(misc_accounts, gov, token, auth, Vault):
-    # mint some tokens to alice
-    alice = misc_accounts[0]
-    token.mint(alice, 10_000e18)
-
+def test_batched_burning_loss(misc_accounts, gov, token, auth, Vault, MockStrategy):
     # deploy the vault
     vault = gov.deploy(Vault)
     vault.initialize(token, auth, ZERO_ADDRESS, ZERO_ADDRESS)
     vault.triggerPause()
     vault.setDepositLimits(MAX_UINT256, MAX_UINT256)
 
-    # should pass
-    balanceBefore = token.balanceOf(alice)
-    exchangeRate = vault.exchangeRate()
-    token.approve(vault, 10_000e18, {"from": alice})
-    vault.deposit(alice, 10_000e18, {"from": alice})
+    for u in misc_accounts:
+        token.mint(u, 1000 * 1e18)
 
-    assert vault.totalFloat() == 10_000e18
-    assert vault.balanceOf(alice) == 10_000e18 * exchangeRate / vault.baseUnit()
-    assert token.balanceOf(alice) == (balanceBefore - 10_000e18)
-    assert token.balanceOf(vault) == 10_000e18
+        token.approve(vault, 1000 * 1e18, {"from": u})
+        vault.deposit(u, 1000 * 1e18, {"from": u})
 
+    for u in misc_accounts:
+        balance = vault.balanceOf(u)
+        vault.enterBatchBurn(balance, {"from": u})
 
-def deposit_after_exchange_rate_change(misc_accounts, gov, token, auth):
-    # mint some tokens to alice
-    [alice, bob] = misc_accounts[0:2]
-    token.mint(alice, 10_000e18)
-    token.mint(bob, 10_000e18)
+    token.burn(vault, 100 * 1e18)
 
-    # deploy the vault
-    vault = gov.deploy(Vault)
-    vault.initialize(token, auth, ZERO_ADDRESS, ZERO_ADDRESS)
-    vault.triggerPause()
-    vault.setDepositLimits(MAX_UINT256, MAX_UINT256)
+    total_shares = vault.totalSupply()
+    exchange_rate = vault.exchangeRate()
 
-    # deploy and set strategy for vault
-    strategy = gov.deploy(MockStrategy)
-    strategy.initialize(vault, token, gov, gov, "MockStrategy")
-    vault.trustStrategy(strategy)
+    vault.execBatchBurn()
 
-    # alice deposits
-    token.approve(vault, 10_000e18, {"from": alice})
-    vault.deposit(alice, 10_000e18, {"from": alice})
-    vault.depositIntoStrategy(10_000e18)
+    batch_burn = vault.batchBurns(1).dict()
+    assert batch_burn["totalShares"] == total_shares
+    assert batch_burn["amountPerShare"] == exchange_rate
 
-    # mint 10% of yield
-    exchangeRateBefore = vault.exchangeRate()
-    token.mint(strategy, int(token.balanceOf(strategy) * 0.1))
-    vault.harvest([strategy])
-
-    assert vault.exchangeRate() > exchangeRateBefore
-
-    # bob deposits after increase
-    token.approve(vault, 10_000e18, {"from": bob})
-    vault.deposit(bob, 10_000e18, {"from": bob})
-    vault.depositIntoStrategy(10_000e18)
-
-    assert vault.balanceOf(bob) < vault.balanceOf(alice)
+    for u in misc_accounts:
+        vault.exitBatchBurn({"from": u})
+        assert token.balanceOf(u) == (1000 * exchange_rate)
--- vaults/tests/unit/vault/test_strategies.py
+++ vaults/tests/unit/vault/test_strategies.py
@@ -65,6 +65,9 @@ def test_can_deposit_underlying_in_trusted_strategy(
     # assert balances
     assert token.balanceOf(vault) == 1e18
     assert token.balanceOf(strategy) == 9e18
+    assert vault.totalFloat() == 1e18
+    assert vault.totalStrategyHoldings() == 9e18
+    assert vault.totalUnderlying() == 1e19
 
 
 def test_cant_deposit_underlying_in_untrusted_strategy(
@@ -87,3 +90,31 @@ def test_cant_deposit_underlying_in_untrusted_strategy(
     # deposit underlying in the strategy
     with brownie.reverts("depositIntoStrategy::UNTRUSTED_STRATEGY"):
         vault.depositIntoStrategy(strategy, 1e18)
+
+
+def test_withdraw_underlying_strategy(gov, token, auth, MockStrategy, Vault):
+    # deploy the vault
+    vault = gov.deploy(Vault)
+    vault.initialize(token, auth, ZERO_ADDRESS, ZERO_ADDRESS)
+    vault.setDepositLimits(MAX_UINT256, MAX_UINT256)
+    vault.triggerPause()
+
+    # deploy the strategy
+    strategy = gov.deploy(MockStrategy)
+    strategy.initialize(vault, token, gov, gov, "MockStrategy")
+
+    # trust the strategy
+    vault.trustStrategy(strategy, {"from": gov})
+
+    # deposit underlying in the vault
+    token.approve(vault, 1e19)
+    vault.deposit(gov, 1e19)
+
+    # deposit underlying in the strategy
+    vault.depositIntoStrategy(strategy, 9e18)
+
+    # withdraw underlying from strategy
+    vault.withdrawFromStrategy(strategy, 9e18)
+
+    assert vault.totalFloat() == 1e19
+    assert vault.totalUnderlying() == 1e19
