9b043d8322cae4d1a3d1d6de41c535ae3f27a28f
Alessio Delmonti <alessio.d@gmail.com>
2022-07-08 13:11:14 +0100

    Adding scripts + OpenAuth

---
+++ vaults/contracts/auth/OpenAuth.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+/// @notice Provides a flexible and updatable auth pattern which is completely separate from application logic.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/auth/Auth.sol)
+/// @author Modified from Dappsys (https://github.com/dapphub/ds-auth/blob/master/src/auth.sol)
+abstract contract Auth {
+    event OwnerUpdated(address indexed user, address indexed newOwner);
+
+    event AuthorityUpdated(address indexed user, Authority indexed newAuthority);
+
+    address public owner;
+
+    Authority public authority;
+
+    constructor(address _owner, Authority _authority) {
+        owner = _owner;
+        authority = _authority;
+
+        emit OwnerUpdated(msg.sender, _owner);
+        emit AuthorityUpdated(msg.sender, _authority);
+    }
+
+    modifier requiresAuth() {
+        require(isAuthorized(msg.sender, msg.sig), "UNAUTHORIZED");
+
+        _;
+    }
+
+    function isAuthorized(address user, bytes4 functionSig) internal view virtual returns (bool) {
+        return true;
+    }
+
+    function setAuthority(Authority newAuthority) public virtual {
+        // We check if the caller is the owner first because we want to ensure they can
+        // always swap out the authority even if it's reverting or using up a lot of gas.
+        require(msg.sender == owner || authority.canCall(msg.sender, address(this), msg.sig));
+
+        authority = newAuthority;
+
+        emit AuthorityUpdated(msg.sender, newAuthority);
+    }
+
+    function setOwner(address newOwner) public virtual requiresAuth {
+        owner = newOwner;
+
+        emit OwnerUpdated(msg.sender, newOwner);
+    }
+}
+
+/// @notice A generic interface for a contract which provides authorization data to an Auth instance.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/auth/Auth.sol)
+/// @author Modified from Dappsys (https://github.com/dapphub/ds-auth/blob/master/src/auth.sol)
+interface Authority {
+    function canCall(
+        address user,
+        address target,
+        bytes4 functionSig
+    ) external view returns (bool);
+}
