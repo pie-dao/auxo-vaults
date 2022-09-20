fcbbadf0500389ce412f86f56c2268c1ff881280
jordan <jordaniza@proton.me>
2022-09-20 20:52:04 +0400

    working beefy vault

--- hub/src/strategy/strategies/beefy/BeefyVelodrome.sol
+++ hub/src/strategy/strategies/beefy/BeefyVelodrome.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.12;
 
 import {ERC20Upgradeable as ERC20} from "@oz-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {OwnableUpgradeable as Ownable} from "@oz-upgradeable/access/OwnableUpgradeable.sol";
@@ -16,19 +16,39 @@ import {BaseStrategy} from "@strategies/base/BaseStrategy.sol";
 
 import {IBeefyVaultV6} from "./interfaces/IBeefyVaultV6.sol";
 import {IBeefyUniV2ZapSolidly} from "./interfaces/IBeefyUniV2ZapSolidly.sol";
+import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
+import {IUniswapRouterSolidly} from "./interfaces/IUniswapV2Router01.sol";
 
 /// @notice deposits USDC into the beefy vault
-contract BeefyVelodromeStrategy is BaseStrategy {
+/// @dev this is for USDC on Optimism
+contract BeefyVelodromeStrategyUSDC_MAI is BaseStrategy {
     using SafeERC20 for IERC20;
 
-    // --- todo: check upgrade
+    // ----------------
+    // Variables
+    // ----------------
+
+    /// @notice address of the Beefy Vault on optimism
     IBeefyVaultV6 public constant BEEFY_VAULT_USDC_MAI =
         IBeefyVaultV6(0x01D9cfB8a9D43013a1FdC925640412D8d2D900F0);
+
+    /// @notice address of the Uniswap Beefy Zap LP contract
     IBeefyUniV2ZapSolidly public constant BEEFY_UNI_V2_ZAP =
         IBeefyUniV2ZapSolidly(
             payable(0x9b50B06B81f033ca86D70F0a44F30BD7E0155737)
         );
 
+    /// @notice the accepted percent slippage in whole percent from trades
+    /// @dev this can be set before calling the depositUnderlying or withdrawUnderlying functions
+    uint8 public slippagePercentage;
+
+    // ----------------
+    // Initializer
+    // ----------------
+
+    /// @param _vault the address of the Auxo Vault that will deposit into this strategy
+    /// @param _manager the address of the manager who can administrate this strategy
+    /// @param _strategist address of the strategist who has limited but elevated access
     function initialize(
         IVault _vault,
         address _manager,
@@ -41,57 +61,131 @@ contract BeefyVelodromeStrategy is BaseStrategy {
             _strategist,
             "BeefyUSDC"
         );
+        slippagePercentage = 3;
     }
 
-    function depositUnderlying(uint256 amount) external override {
+    // ----------------
+    // State Changing Functions
+    // ----------------
+
+    /// @notice updates the slippage percentage that is permitted in swaps
+    /// @dev slippage is calculated (x * (100 - slippagePercentage) / 100)
+    /// @param _slippagePercentage the new slippage percentage
+    function setSlippage(uint8 _slippagePercentage) external {
         require(
             msg.sender == manager,
-            "BeefyVelodromeStrategy::depositUnderling:NOT MANAGER"
+            "BeefyVelodromeStrategy::setSlippage:NOT MANAGER"
+        );
+        require(
+            _slippagePercentage < 100,
+            "BeefyVelodromeStrategy::setSlippage:INVALID SLIPPAGE"
+        );
+        slippagePercentage = _slippagePercentage;
+    }
+
+    /// @notice deposits underlying tokens held in the strategy into the beefy vault
+    /// @param _amount of the underlying token to deposit to the beefy vault
+    function depositUnderlying(uint256 _amount) external override {
+        require(
+            msg.sender == manager,
+            "BeefyVelodromeStrategy::depositUnderlying:NOT MANAGER"
         );
 
-        underlying.approve(address(BEEFY_UNI_V2_ZAP), amount);
+        underlying.approve(address(BEEFY_UNI_V2_ZAP), _amount);
 
         BEEFY_UNI_V2_ZAP.beefIn(
             address(BEEFY_VAULT_USDC_MAI),
-            (amount * 995) / 1000,
+            (_amount * (100 - slippagePercentage)) / 100,
             address(underlying),
-            amount
+            _amount
         );
+        emit DepositUnderlying(_amount);
     }
 
-    function withdrawUnderlying(uint256 amount) external override {
+    /// @notice withdraws underlying tokens from the vault into the strategy
+    /// @param _amountShares the number of beefy vault shares to burn
+    function withdrawUnderlying(uint256 _amountShares) external override {
         require(
             msg.sender == manager,
-            "BeefyVelodromeStrategy::depositUnderling:NOT MANAGER"
+            "BeefyVelodromeStrategy::withdrawUnderlying:NOT MANAGER"
         );
-        BEEFY_VAULT_USDC_MAI.approve(address(BEEFY_UNI_V2_ZAP), amount);
+        BEEFY_VAULT_USDC_MAI.approve(address(BEEFY_UNI_V2_ZAP), _amountShares);
 
-        BEEFY_UNI_V2_ZAP.beefOut(address(BEEFY_VAULT_USDC_MAI), amount);
+        // Min quantity of underlying to accept from swap (assuming 50:50 pool)
+        uint256 minUnderlyingFromPool = (sharesToUnderlying(_amountShares / 2) *
+            (100 - slippagePercentage)) / 100;
+
+        BEEFY_UNI_V2_ZAP.beefOutAndSwap(
+            address(BEEFY_VAULT_USDC_MAI),
+            _amountShares,
+            address(underlying),
+            minUnderlyingFromPool
+        );
+        emit WithdrawUnderlying(_amountShares);
     }
 
+    // ----------------
+    // View Functions
+    // ----------------
+
+    /// @notice estimates the value of the underlying holdings
     function estimatedUnderlying() external view override returns (uint256) {
         return float() + beefyBalance();
     }
 
-    function depositedIntoVault() public view returns (uint256) {
-        return depositedUnderlying - float();
+    /// @notice estimates the value of beefy vault shares in underlying tokens
+    /// @dev takes shares * price and gets the amount of tokens returned from uni pair
+    /// @dev then gets a quote to swap the pair token we don't need
+    /// @param _shares the number of shares to estimate a value for
+    function sharesToUnderlying(uint256 _shares) public view returns (uint256) {
+        // price * qty of shares is the total liqudity we want to quote for
+        uint256 liquidityToRemove = (_shares *
+            BEEFY_VAULT_USDC_MAI.getPricePerFullShare()) /
+            10**BEEFY_VAULT_USDC_MAI.decimals();
+
+        // grab the pair from the vault
+        IUniswapV2Pair pair = IUniswapV2Pair(BEEFY_VAULT_USDC_MAI.want());
+        address token0 = pair.token0();
+        address token1 = pair.token1();
+
+        // set the swap token to the pair value we are not interested in
+        address swapToken = (token0 == address(underlying)) ? token1 : token0;
+
+        IUniswapRouterSolidly router = IUniswapRouterSolidly(
+            BEEFY_UNI_V2_ZAP.router()
+        );
+
+        // we have vault balance and price, we now want to actually get the quotes
+        (
+            uint256 receiveAmountSwapToken,
+            uint256 receiveAmountUnderlying
+        ) = router.quoteRemoveLiquidity(
+                swapToken,
+                address(underlying),
+                true,
+                liquidityToRemove
+            );
+
+        // we only want a single underlying token
+        // so quote for swapping the other pair token for it
+        (uint256 swappedAmount, ) = router.getAmountOut(
+            receiveAmountSwapToken,
+            swapToken,
+            address(underlying)
+        );
+        return swappedAmount + receiveAmountUnderlying;
     }
 
     /// @notice computes the estimated balance in underlying
-
     /// we need to approximate the value of the beefy vault in whatever unerlying token we have
     function beefyBalance() public view returns (uint256) {
-        bytes memory sig = abi.encodeWithSignature(
-            "beefOutAndSwap(address,uint256,address,uint256)",
-            address(BEEFY_VAULT_USDC_MAI),
-            (depositedIntoVault() * 995) / 1000,
-            address(underlying),
-            depositedIntoVault()
-        );
+        uint256 balanceStrat = BEEFY_VAULT_USDC_MAI.balanceOf(address(this));
+        return sharesToUnderlying(balanceStrat);
+    }
 
-        (bool success, bytes memory data) = address(BEEFY_UNI_V2_ZAP)
-            .staticcall(sig);
-        require(success, "Call failed");
-        return 100;
+    /// @dev beefy zaps can return trace amounts of unwanted token to the contract
+    /// @param _token the address of the token pair that is not underlying
+    function _residualBalance(address _token) internal view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
     }
 }
--- hub/test/strategies/beefy/Beefy.t.sol
+++ hub/test/strategies/beefy/Beefy.t.sol
@@ -18,7 +18,7 @@ import {StargateRouterMock} from "@hub-test/mocks/MockStargateRouter.sol";
 import {TransparentUpgradeableProxy} from "@oz/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "@oz/proxy/transparent/ProxyAdmin.sol";
 
-import {BeefyVelodromeStrategy} from "@strategies/beefy/BeefyVelodrome.sol";
+import {BeefyVelodromeStrategyUSDC_MAI} from "@strategies/beefy/BeefyVelodrome.sol";
 import {IBeefyVaultV6} from "@strategies/beefy/interfaces/IBeefyVaultV6.sol";
 
 // for testing strategies, we don't need the full vault functionality
@@ -39,15 +39,26 @@ interface IERC20Mintable is IERC20 {
 
 /// @dev IMPORTANT run this test against a fork of the optimism network
 contract TestBeefyVelodromeStrategy is PRBTest {
+    bool DEBUG = true;
+
     IVault vault;
-    BeefyVelodromeStrategy beefyStrategy;
+    BeefyVelodromeStrategyUSDC_MAI beefyStrategy;
 
     IERC20Mintable constant usdc_optimism =
         IERC20Mintable(0x7F5c764cBc14f9669B88837ca1490cCa17c31607);
 
+    IERC20 constant mai_optimism =
+        IERC20(0xdFA46478F9e5EA86d57387849598dbFB2e964b02);
+
     address constant manager = 0xBEeFbeefbEefbeEFbeEfbEEfBEeFbeEfBeEfBeef;
     address constant strategist = 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B;
     ProxyAdmin admin;
+
+    modifier asManager() {
+        vm.startPrank(manager);
+        _;
+        vm.stopPrank();
+    }
 
     function _setupVault(IERC20 _token) internal returns (IVault) {
         SimpleMockVault mockVault = new SimpleMockVault(_token);
@@ -56,30 +67,26 @@ contract TestBeefyVelodromeStrategy is PRBTest {
 
     function initializeBeefyContractProxy(IVault _vault, ProxyAdmin _admin)
         internal
-        returns (BeefyVelodromeStrategy)
+        returns (BeefyVelodromeStrategyUSDC_MAI)
     {
-        address implementation = address(new BeefyVelodromeStrategy());
+        address implementation = address(new BeefyVelodromeStrategyUSDC_MAI());
 
         TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
             implementation,
             address(_admin),
             abi.encodeWithSelector(
-                BeefyVelodromeStrategy.initialize.selector,
+                BeefyVelodromeStrategyUSDC_MAI.initialize.selector,
                 _vault,
                 manager,
                 strategist
             )
         );
-        return BeefyVelodromeStrategy(address(proxy));
-    }
-
-    modifier asManager() {
-        vm.startPrank(manager);
-        _;
-        vm.stopPrank();
+        return BeefyVelodromeStrategyUSDC_MAI(address(proxy));
     }
 
     function setUp() public {
+        if (DEBUG) console.log("---- DEBUG MODE IS ON ----");
+
         vault = _setupVault(usdc_optimism);
         admin = new ProxyAdmin();
 
@@ -92,46 +99,118 @@ contract TestBeefyVelodromeStrategy is PRBTest {
         vm.startPrank(address(vault));
 
         usdc_optimism.approve(address(beefyStrategy), 1e18);
-        beefyStrategy.deposit(1e10);
+        beefyStrategy.deposit(1e18);
 
         vm.stopPrank();
     }
 
     function testDepositIntoStrategy() public {
-        assertEq(beefyStrategy.float(), 1e10);
+        assertEq(beefyStrategy.float(), 1e18);
+        assertAlmostEq(beefyStrategy.estimatedUnderlying(), 1e18, 1e12);
     }
 
     // tests to see if deposit successful
-    function testDepositToVault() public asManager {
-        beefyStrategy.depositUnderlying(1e10);
+    function testDepositToVault(uint256 _amt) public asManager {
+        vm.assume(_amt < 1e12 && _amt > 1000);
+        logTokenBalances(address(beefyStrategy));
+
+        beefyStrategy.depositUnderlying(_amt);
 
         // check no underlying left in the strat
-        assertEq(beefyStrategy.float(), 0);
-        assertEq(usdc_optimism.balanceOf(address(beefyStrategy)), 0);
+        logTokenBalances(address(beefyStrategy));
+
+        // rounding error in the beef in: allow 0.001c error max
+        assertAlmostEq(beefyStrategy.float(), 1e18 - _amt, 1e3);
 
         // check that the balance of the strat in beefy tokens has increased
         IBeefyVaultV6 beefyVault = beefyStrategy.BEEFY_VAULT_USDC_MAI();
-        assertNotEq(beefyVault.balanceOf(address(beefyStrategy)), 0);
+
+        // beefy has 18 decimals, so 1e12 is 1 millionth of a token
+        assertAlmostEq(
+            beefyStrategy.sharesToUnderlying(
+                beefyVault.balanceOf(address(beefyStrategy))
+            ),
+            _amt,
+            1e12
+        );
+
+        // underlying not changed
+        assertAlmostEq(beefyStrategy.estimatedUnderlying(), 1e18, 1e12);
     }
 
-    // tests to see if deposit successful
-    function testWithdrawFromVault() public asManager {
-        beefyStrategy.depositUnderlying(1e10);
+    // useful for debugging
+    function logTokenBalances(address _who) internal view {
+        if (DEBUG) {
+            IBeefyVaultV6 beefyVault = beefyStrategy.BEEFY_VAULT_USDC_MAI();
+
+            console.log("---- Token Balances -----");
+            console.log("Beefy Vault", beefyVault.balanceOf(_who));
+            console.log("USDC", usdc_optimism.balanceOf(_who) / 10**6);
+            console.log("MAI", mai_optimism.balanceOf(_who) / 10**18);
+        }
+    }
+
+    function testWithdrawFromVault(uint8 _withdrawPc) public asManager {
+        vm.assume(_withdrawPc <= 100 && _withdrawPc > 0);
         IBeefyVaultV6 beefyVault = beefyStrategy.BEEFY_VAULT_USDC_MAI();
+
+        logTokenBalances(address(beefyStrategy));
+
+        beefyStrategy.depositUnderlying(1_000_000);
+
         uint256 balanceBefore = beefyVault.balanceOf(address(beefyStrategy));
-
-        require(balanceBefore >= 1e10, "balancetoo low");
-
-        beefyStrategy.withdrawUnderlying(1e10);
-
-        // check no underlying left in the strat
-        assertNotEq(beefyStrategy.float(), 0);
-        assertNotEq(usdc_optimism.balanceOf(address(beefyStrategy)), 0);
-
-        // check that the balance of the strat in beefy tokens has increased
-        assertNotEq(
-            beefyVault.balanceOf(address(beefyStrategy)),
+        uint256 approxValueBefore = beefyStrategy.sharesToUnderlying(
             balanceBefore
         );
+
+        logTokenBalances(address(beefyStrategy));
+
+        uint256 sharesWithdraw = (balanceBefore * _withdrawPc) / 100;
+
+        beefyStrategy.withdrawUnderlying(sharesWithdraw);
+
+        logTokenBalances(address(beefyStrategy));
+
+        assertAlmostEq(beefyStrategy.estimatedUnderlying(), 1e18, 1e12);
+
+        uint256 balanceAfter = beefyVault.balanceOf(address(beefyStrategy));
+        uint256 approxValueAfter = beefyStrategy.sharesToUnderlying(
+            balanceAfter
+        );
+
+        assertAlmostEq(
+            approxValueBefore - approxValueAfter,
+            beefyStrategy.sharesToUnderlying(sharesWithdraw),
+            1e12
+        );
+    }
+
+    function testSlippage(uint8 _slippagePercentage) public asManager {
+        if (_slippagePercentage < 100) {
+            beefyStrategy.setSlippage(_slippagePercentage);
+            assertEq(beefyStrategy.slippagePercentage(), _slippagePercentage);
+        } else {
+            vm.expectRevert(
+                "BeefyVelodromeStrategy::setSlippage:INVALID SLIPPAGE"
+            );
+            beefyStrategy.setSlippage(_slippagePercentage);
+        }
+    }
+
+    function testManagerialFunctions(address _notManager) public {
+        vm.assume(_notManager != manager);
+
+        vm.expectRevert(
+            "BeefyVelodromeStrategy::withdrawUnderlying:NOT MANAGER"
+        );
+        beefyStrategy.withdrawUnderlying(100);
+
+        vm.expectRevert(
+            "BeefyVelodromeStrategy::depositUnderlying:NOT MANAGER"
+        );
+        beefyStrategy.depositUnderlying(100);
+
+        vm.expectRevert("BeefyVelodromeStrategy::setSlippage:NOT MANAGER");
+        beefyStrategy.setSlippage(100);
     }
 }
